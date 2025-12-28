53ec949c734165c910b9cbea80567970b410537d
Saeed Mzr <saeedmouzarmi@gmail.com>
2025-12-28 21:50:43 +0330

    Added admin portal for user model.

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,38 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
 
-# Register your models here.
+
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    list_display = ('email', 'first_name', 'last_name', 'is_staff', 'is_active', 'date_joined')
+    list_filter = ('is_staff', 'is_superuser', 'is_active', 'date_joined')
+    search_fields = ('email', 'first_name', 'last_name')
+    ordering = ('-date_joined',)
+
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Personal info', {'fields': ('first_name', 'last_name')}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+        ('Important dates', {'fields': ('last_login', 'date_joined')}),
+    )
+
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password1', 'password2', 'first_name', 'last_name'),
+        }),
+    )
+
+    readonly_fields = ('date_joined', 'last_login')
+
+    def get_form(self, request, obj=None, **kwargs):
+        form = super().get_form(request, obj, **kwargs)
+        form.base_fields['username'].widget = admin.widgets.AdminTextInput(
+            attrs={'readonly': 'readonly', 'style': 'display:none;'}
+        )
+        return form
+
+
+# Unregister default User, register CustomUser
+admin.site.register(CustomUser, CustomUserAdmin)
